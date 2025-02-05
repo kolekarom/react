24708fb6506e8736371af1c3731183402b99e539
kolekarom <omkolekar72@gmail.com>
2025-02-05 21:01:39 +0530

    second change

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      my name is om 
+    </>
+  )
+}
+
+export default App
